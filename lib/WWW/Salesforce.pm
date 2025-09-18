65e9fa2eb5fc21ebe0cb2214d0324025f04d4489
Chase Whitener <cwhitener@gmail.com>
2025-09-18 17:57:48 -0400

    Bump version

--- lib/WWW/Salesforce.pm
+++ lib/WWW/Salesforce.pm
@@ -13,7 +13,7 @@ use WWW::Salesforce::Constants;
 use WWW::Salesforce::Deserializer;
 use WWW::Salesforce::Serializer;
 
-our $VERSION = '0.400';
+our $VERSION = '0.401';
 
 our $SF_PROXY       = 'https://login.salesforce.com/services/Soap/u/64.0';
 our $SF_URI         = 'urn:partner.soap.sforce.com';
